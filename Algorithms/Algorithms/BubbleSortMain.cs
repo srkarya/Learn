2bf52278eec9c155c7eaa93118c09d8f3fd9b1c4
srkarya <vgsl.sriki@gmail.com>
2016-05-21 23:49:40 -0700

    Update Bubble sort

--- Algorithms/Algorithms/BubbleSortMain.cs
+++ Algorithms/Algorithms/BubbleSortMain.cs
@@ -23,39 +23,39 @@ namespace Algorithms
 
 		#region Public methods
 
-		/// <summary>
-		/// Method execution starts here.
-		/// </summary>
-		/// <param name="args">Input arguments</param>
-		public static void Main(string[] args)
-		{
-			BubbleSort bubblleSort = new BubbleSort();
-			Console.WriteLine("Ënter array size");
-			var inputSize = Console.ReadLine().Trim();
-			var size = Convert.ToInt32(string.IsNullOrEmpty(inputSize) ? zero : inputSize);
-			var arr = new int[size];
-			Console.WriteLine("Enter array item/element(s)");
+		///// <summary>
+		///// Method execution starts here.
+		///// </summary>
+		///// <param name="args">Input arguments</param>
+		//public static void Main(string[] args)
+		//{
+		//	BubbleSort bubblleSort = new BubbleSort();
+		//	Console.WriteLine("Ënter array size");
+		//	var inputSize = Console.ReadLine().Trim();
+		//	var size = Convert.ToInt32(string.IsNullOrEmpty(inputSize) ? zero : inputSize);
+		//	var arr = new int[size];
+		//	Console.WriteLine("Enter array item/element(s)");
 
-			for (var i = 0; i < size; i++)
-			{
-				var userInput = Console.ReadLine().Trim();
-				arr[i] = Convert.ToInt32(string.IsNullOrEmpty(userInput) ? zero : userInput);
-			}
+		//	for (var i = 0; i < size; i++)
+		//	{
+		//		var userInput = Console.ReadLine().Trim();
+		//		arr[i] = Convert.ToInt32(string.IsNullOrEmpty(userInput) ? zero : userInput);
+		//	}
 
-			if (arr.Length > 0)
-			{
-				//bubblleSort.Sort(arr);
-				bubblleSort.OptimizedSort(arr);
-				Console.WriteLine("Sorted array:");
-				Print(arr);
-				Console.Read();
-			}
-			else
-			{
-				Console.WriteLine("Press any key to exit.");
-				Console.Read();
-			}
-		}
+		//	if (arr.Length > 0)
+		//	{
+		//		//bubblleSort.Sort(arr);
+		//		bubblleSort.OptimizedSort(arr);
+		//		Console.WriteLine("Sorted array:");
+		//		Print(arr);
+		//		Console.Read();
+		//	}
+		//	else
+		//	{
+		//		Console.WriteLine("Press any key to exit.");
+		//		Console.Read();
+		//	}
+		//}
 
 		#endregion
 
