0cf1096c35654d4a29aa4274c62a1ac23ee989f7
srkarya <vgsl.sriki@gmail.com>
2016-05-21 23:45:34 -0700

    Insertion Sort

---
+++ Algorithms/Algorithms.Sort/InsertionSort.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms.Sort
+{
+	#region Public methods
+
+	/// <summary>
+	/// Class Insertion Sort
+	/// </summary>
+	public class InsertionSort
+	{
+		#region Sort
+
+		/// <summary>
+		/// To sort integer array
+		/// </summary>
+		/// <param name="arr">Integer array</param>
+		public void Sort(int[] arr)
+		{
+			int n = arr.Length;
+			for (var i = 1; i < n; ++i)
+			{
+				var j = i - 1;
+				var key = arr[i];
+				while (j >= 0 && arr[j] > key)
+				{
+					arr[j + 1] = arr[j];
+					j = j - 1;
+				}
+				arr[j + 1] = key;
+			}
+		}
+
+		#endregion
+	}
+
+	#endregion
+}
---
+++ Algorithms/Algorithms/InsertionSortMain.cs
@@ -0,0 +1,79 @@
+﻿#region Namespace
+using Algorithms.Sort;
+using System;
+#endregion
+
+namespace Algorithms
+{
+	#region Class Insertion Sort
+
+	/// <summary>
+	/// Class Insertion Sort
+	/// </summary>
+	public class InsertionSortMain
+	{
+		#region Private constants
+
+		/// <summary>
+		/// Constant
+		/// </summary>
+		private const string zero = "0";
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Method execution starts here.
+		/// </summary>
+		/// <param name="args">Input arguments</param>
+		public static void Main(string[] args)
+		{
+			InsertionSort inst = new InsertionSort();
+			Console.WriteLine("Ënter array size");
+			var inputSize = Console.ReadLine().Trim();
+			var size = Convert.ToInt32(string.IsNullOrEmpty(inputSize) ? zero : inputSize);
+			var arr = new int[size];
+			Console.WriteLine("Enter array item/element(s)");
+
+			for (var i = 0; i < size; i++)
+			{
+				var userInput = Console.ReadLine().Trim();
+				arr[i] = Convert.ToInt32(string.IsNullOrEmpty(userInput) ? zero : userInput);
+			}
+
+			if (arr.Length > 0)
+			{
+				inst.Sort(arr);
+				Console.WriteLine("Sorted array:");
+				Print(arr);
+				Console.Read();
+			}
+			else
+			{
+				Console.WriteLine("Press any key to exit.");
+				Console.Read();
+			}
+		}
+
+		#endregion
+
+		#region Private methods
+
+		/// <summary>
+		/// To print an array.
+		/// </summary>
+		/// <param name="arr">Integer array</param>
+		private static void Print(int[] arr)
+		{
+			for (var i = 0; i < arr.Length; i++)
+			{
+				Console.Write(arr[i] + " ");
+			}
+		}
+
+		#endregion
+	}
+
+	#endregion
+}
